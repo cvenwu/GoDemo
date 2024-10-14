9148f5c2d02625974678dfc1aa3b1eb87d3b0c21
cvenwu <yirufeng@foxmail.com>
2024-10-14 19:51:59 +0800

    [feat]初始化

---
+++ main.go
@@ -0,0 +1,16 @@
+/*
+ * @Author: cvenwu yirufeng@foxmail.com
+ * @Date: 2024-10-14 19:50:43
+ * @LastEditors: cvenwu yirufeng@foxmail.com
+ * @LastEditTime: 2024-10-14 19:51:25
+ * @Description:
+ *
+ * Copyright (c) 2024 by yirufeng@foxmail.com, All Rights Reserved.
+ */
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("123")
+}
